0a6989ed4eeea8b104790a29f8fc742b35e0d801
dfirtnt <12226521+dfirtnt@users.noreply.github.com>
2026-01-07 11:11:46 -0500

    Remove Workshop/ from git tracking

--- Workshop/training/train_secbert.py
+++
@@ -1,8 +0,0 @@
-"""Train SecBERT span extractor."""
-
-from Workshop.training.train_common import run_training
-from Workshop.utilities.tokenizer_utils import MODEL_PRESETS
-
-
-if __name__ == "__main__":
-    run_training("secbert", MODEL_PRESETS["secbert"])
